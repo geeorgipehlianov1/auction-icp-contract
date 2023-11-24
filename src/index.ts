f49f2ec25cff2f4e43b1c2142210174416de4c3a
Georgi Pehlivanov <george9040@abv.bg>
2023-11-24 10:30:12 +0200

    Applay some changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt, Principal } from 'azle';
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 type Auction = Record<{
@@ -6,29 +6,25 @@ type Auction = Record<{
     assetType: string;
     assetDescription: string;
     ownerName: string;
-    owner: Principal;
+    ownerId: string;
     startDate: nat64;
     endDate: nat64;
-    status: AuctionStatus
+    status: string
 }>
 
 type AuctionPayload = Record<{
     assetType: string;
     assetDescription: string;
     ownerName: string;
-    status: AuctionStatus
+    status: string
 }>
 
-enum AuctionStatus {
-    active = 'active', 
-    inactive = 'inactive'   
-}
 
 const fixedEndDate = 86400000;
 
-const auctionStorage = new StableBTreeMap<string, Auction>(0, 44, 1024);
+const auctionStorage = new StableBTreeMap<string, Auction>(1, 44, 1024);
 
-$query
+$query;
 export function getAllAuctions(): Result<Vec<Auction>, string>
 {
     try {
@@ -38,7 +34,7 @@ export function getAllAuctions(): Result<Vec<Auction>, string>
     }
 }
 
-$query
+$query;
 export function getAuctionById(auctionId: string): Result<Auction, string> {
     return match(auctionStorage.get(auctionId), {
         Some: (auction) => Result.Ok<Auction, string>(auction),
@@ -46,17 +42,17 @@ export function getAuctionById(auctionId: string): Result<Auction, string> {
     })
 }
 
-$query
-export function getOwnersAuctions(): Result<Vec<Auction>, string> {
+$query;
+export function getOwnersAuctions(ownerId: string): Result<Vec<Auction>, string> {
     try {
-        return Result.Ok(auctionStorage.values().filter((auction) => auction.owner.toString() === ic.caller.toString()));   
+        return Result.Ok(auctionStorage.values().filter((auction) => auction.ownerId === ownerId));   
     } catch (error) {
         return Result.Err(`Failed to retrieve your auctions!`)
     }
 }
 
-$query
-export function getAuctionsByStatus(status: AuctionStatus): Result<Vec<Auction>, string> {
+$query;
+export function getAuctionsByStatus(status: string): Result<Vec<Auction>, string> {
     try {
         if (!isAuctionStatusValid(status)) {
             return Result.Err(`Invalid auction status: ${status}`)
@@ -73,33 +69,14 @@ export function getAuctionsByStatus(status: AuctionStatus): Result<Vec<Auction>,
   
 }
 
-$query
-export function getActiveAuctions(): Result<Vec<Auction>, string>
-{ 
-    try {
-        return Result.Ok<Vec<Auction>, string>(auctionStorage.values().filter((auction) => auction.status === AuctionStatus.active))
-    } catch (error) {
-        return Result.Err(`Failed to retrieve auctions with ${AuctionStatus.active} status`)
-    }
-}
-
-$query
-export function getExpiredAuctions(): Result<Vec<Auction>, string> {
-    try {
-        return Result.Ok<Vec<Auction>, string>(auctionStorage.values().filter((auction) => auction.status === AuctionStatus.inactive))
-    } catch (error) {
-        return Result.Err(`Failed to retrieve auctions with ${AuctionStatus.inactive} status`)
-    }
-}
-
-$update
+$update;
 export function createAuction(payload: AuctionPayload): Result<Auction, string> {
     try {
         if (!payload.assetType || !payload.assetDescription || !payload.ownerName || !payload.status) {
             return Result.Err<Auction, string>('Incomplete input data!');
         } 
 
-        const auction: Auction = { id: uuidv4(), startDate: ic.time(), owner: ic.caller(), endDate: ic.time() + BigInt(fixedEndDate), ...payload };
+        const auction: Auction = { id: uuidv4(), startDate: ic.time(), ownerId: uuidv4(), endDate: ic.time() + BigInt(fixedEndDate), ...payload };
         auctionStorage.insert(auction.id, auction);
         return Result.Ok(auction);   
     } catch (error) {
@@ -107,11 +84,11 @@ export function createAuction(payload: AuctionPayload): Result<Auction, string> 
     }
 }
 
-$update
-export function updateAuction(auctionId: string, payload: AuctionPayload): Result<Auction, string> {
+$update;
+export function updateAuction(auctionId: string, ownerId: string, payload: AuctionPayload): Result<Auction, string> {
     return match(auctionStorage.get(auctionId), {
         Some: (auction) => {
-            if (auction.owner.toString() !== ic.caller().toString()) {
+            if (auction.ownerId !== ownerId) {
                 return Result.Err<Auction, string>('Only the owner can update this auction!');
             }
             const updatedAuction: Auction = { ...auction, ...payload };
@@ -122,11 +99,11 @@ export function updateAuction(auctionId: string, payload: AuctionPayload): Resul
     });
 }
 
-$update
-export function endAuction(auctionId: string): Result<Auction, string> { 
+$update;
+export function endAuction(auctionId: string, ownerId: string): Result<Auction, string> { 
     return match(auctionStorage.get(auctionId), {
         Some: (auction) => {
-            if (auction.owner.toString() !== ic.caller().toString()) {
+            if (auction.ownerId !== ownerId) {
                 return Result.Err<Auction, string>('Only the owner can end this auction!');
             }
 
@@ -134,7 +111,7 @@ export function endAuction(auctionId: string): Result<Auction, string> {
                 return Result.Err<Auction, string>('Auction already ended!')
             }
 
-            const endedAuction: Auction = { ...auction, endDate: ic.time(), status: AuctionStatus.inactive };
+            const endedAuction: Auction = { ...auction, endDate: ic.time(), status: 'inactive' };
             auctionStorage.insert(auction.id, endedAuction);
             return Result.Ok<Auction, string>(endedAuction);
         },
@@ -142,11 +119,11 @@ export function endAuction(auctionId: string): Result<Auction, string> {
     });
 }
 
-$update
-export function deleteAuction(auctionId: string): Result<Auction, string> { 
+$update;
+export function deleteAuction(auctionId: string, ownerId: string): Result<Auction, string> { 
     return match(auctionStorage.remove(auctionId), {
         Some: (auction) => {
-            if (auction.owner.toString() !== ic.caller().toString()) {
+            if (auction.ownerId !== ownerId) {
                 return Result.Err<Auction, string>('Only owner can delete auction!');
             }
             return Result.Ok<Auction, string>(auction);
@@ -155,8 +132,8 @@ export function deleteAuction(auctionId: string): Result<Auction, string> {
     });
 }
 
-export function isAuctionStatusValid(status: AuctionStatus) {
-    return status === AuctionStatus.active || status !== AuctionStatus.inactive
+function isAuctionStatusValid(status: string): boolean {
+    return status === 'active' || status === 'inactive'
 }
 
 globalThis.crypto = {
